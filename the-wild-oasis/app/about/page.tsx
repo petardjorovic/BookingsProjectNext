ac8510b0c615267b1735df5bb47f18427d988666
petardjorovic <perodjorovic@gmail.com>
2025-07-12 16:57:23 +0200

    NextJs init the-wild-oasis project

---
+++ the-wild-oasis/app/about/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1>About The wild oasis</h1>;
+}
---
+++ the-wild-oasis/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1 className="text-3xl">The Wild Oasis. Welcome to paradise.</h1>;
+}
