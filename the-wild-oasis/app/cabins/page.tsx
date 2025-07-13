083e941dec6f9fe5ddd6200abe07573d57b35938
petardjorovic <perodjorovic@gmail.com>
2025-07-13 18:22:42 +0200

    Server and Client component

--- the-wild-oasis/app/cabins/page.tsx
+++ the-wild-oasis/app/cabins/page.tsx
@@ -1,3 +1,43 @@
-export default function Page() {
-  return <h1>Cabins page</h1>;
+import Counter from "../components/Counter";
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  if (!res.ok) throw new Error("Failed to fetch users.");
+  const users: User[] = await res.json();
+
+  return (
+    <div>
+      <h1>Cabins page</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+      <Counter data={users} />
+    </div>
+  );
 }
