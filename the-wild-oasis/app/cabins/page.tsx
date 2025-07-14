5bb914af681378548a0a5e81def385909e7050a1
petardjorovic <perodjorovic@gmail.com>
2025-07-14 23:53:45 +0200

    Styles setup

--- the-wild-oasis/app/cabins/page.tsx
+++ the-wild-oasis/app/cabins/page.tsx
@@ -1,43 +1,7 @@
-import Counter from "../components/Counter";
-
-export interface User {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-      lat: string;
-      lng: string;
-    };
-  };
-  phone: string;
-  website: string;
-  company: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
-  };
-}
-
-export default async function Page() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  if (!res.ok) throw new Error("Failed to fetch users.");
-  const users: User[] = await res.json();
-
+export default function Page() {
   return (
     <div>
       <h1>Cabins page</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-      <Counter data={users} />
     </div>
   );
 }
