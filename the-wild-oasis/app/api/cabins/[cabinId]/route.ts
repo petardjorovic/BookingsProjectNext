c079f930821f04b55f74850e2460bf0bb3fff3ed
petardjorovic <perodjorovic@gmail.com>
2025-07-23 21:37:29 +0200

    API route handler

---
+++ the-wild-oasis/app/api/cabins/[cabinId]/route.ts
@@ -0,0 +1,21 @@
+import { getBookedDatesByCabinId, getCabin } from "@/app/_lib/data-service";
+
+type GetProps = {
+  params: Promise<{
+    cabinId: string;
+  }>;
+};
+
+export async function GET(request: Request, { params }: GetProps) {
+  const { cabinId } = await params;
+
+  try {
+    const [cabin, bookedDates] = await Promise.all([
+      getCabin(Number(cabinId)),
+      getBookedDatesByCabinId(Number(cabinId)),
+    ]);
+    return Response.json({ cabin, bookedDates });
+  } catch {
+    return Response.json({ message: "Cabin not found" });
+  }
+}
